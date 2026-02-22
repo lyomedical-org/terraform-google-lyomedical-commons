b7a9501a2d8fcfe27f3c0b10481e6a0e5b8f26b3
pc2 <jearangoo@unal.edu.co>
2026-02-22 12:26:35 -0500

    fixes

--- service_accounts_run.tf
+++ service_accounts_run.tf
@@ -97,4 +97,104 @@ resource "google_project_iam_member" "run_test_public_secretmanager_secret_acces
 	member="serviceAccount:${google_service_account.run_test_public.email}"
 }
 
+resource "google_service_account" "run_production_public" {
+	project=google_project.production_public.project_id
+	account_id="cloud-run"
+}
+resource "google_project_iam_member" "serverless_robot_production_vpcaccess_user_on_svpc_host" {
+	project=google_project.svpc_host.project_id
+	role="roles/vpcaccess.user"
+	member="serviceAccount:service-${google_project.production_public.number}@serverless-robot-prod.iam.gserviceaccount.com"
+	depends_on=[
+		google_project_service.cloudfunctions_production_public,
+		google_project_service.run_production_public
+	]
+}
+resource "google_project_iam_member" "serverless_robot_cloudfunctions_developer_on_production" {
+	project=google_project.production.project_id
+	role="roles/cloudfunctions.developer"
+	member="serviceAccount:service-${google_project.production_public.number}@serverless-robot-prod.iam.gserviceaccount.com"
+}
+resource "google_project_iam_member" "gcf_agent_project_storage_viewer_on_production" {
+	project=google_project.production.project_id
+	role="roles/storage.objectViewer"
+	member="serviceAccount:service-${google_project.production_public.number}@gcf-admin-robot.iam.gserviceaccount.com"
+}
+resource "google_project_iam_member" "gcf_agent_cloudfunctions_developer_on_production" {
+	project=google_project.production.project_id
+	role="roles/cloudfunctions.developer"
+	member="serviceAccount:service-${google_project.production_public.number}@gcf-admin-robot.iam.gserviceaccount.com"
+}
+resource "google_project_iam_member" "run_production_public_iam_service_account_user_on_production_public" {
+	project=google_project.production_public.project_id
+	role="roles/iam.serviceAccountUser"
+	member="serviceAccount:${google_service_account.run_production_public.email}"
+}
+resource "google_project_iam_member" "run_production_public_iam_service_account_user_on_svpc_host" {
+	project=google_project.svpc_host.project_id
+	role="roles/iam.serviceAccountUser"
+	member="serviceAccount:${google_service_account.run_production_public.email}"
+}
+resource "google_project_iam_member" "run_production_public_compute_network_user_on_svpc_host" {
+	project=google_project.svpc_host.project_id
+	role="roles/compute.networkUser"
+	member="serviceAccount:${google_service_account.run_production_public.email}"
+}
+resource "google_project_iam_member" "run_production_public_logging_log_writer_on_production" {
+	project=google_project.production.project_id
+	role="roles/logging.logWriter"
+	member="serviceAccount:${google_service_account.run_production_public.email}"
+}
+resource "google_project_iam_member" "run_production_public_logging_log_writer_on_production_public" {
+	project=google_project.production_public.project_id
+	role="roles/logging.logWriter"
+	member="serviceAccount:${google_service_account.run_production_public.email}"
+}
+resource "google_project_iam_member" "run_production_public_storage_object_admin_on_production" {
+	project=google_project.production.project_id
+	role="roles/storage.objectAdmin"
+	member="serviceAccount:${google_service_account.run_production_public.email}"
+}
+resource "google_project_iam_member" "run_production_public_storage_object_admin_on_production_public" {
+	project=google_project.production_public.project_id
+	role="roles/storage.objectAdmin"
+	member="serviceAccount:${google_service_account.run_production_public.email}"
+}
+resource "google_project_iam_member" "run_production_public_artifactregistry_admin_on_production" {
+	project=google_project.production.project_id
+	role="roles/artifactregistry.admin"
+	member="serviceAccount:${google_service_account.run_production_public.email}"
+}
+resource "google_project_iam_member" "run_production_public_cloudbuild_builds_builder_on_svpc_host" {
+	project=google_project.svpc_host.project_id
+	role="roles/cloudbuild.builds.builder"
+	member="serviceAccount:${google_service_account.run_production_public.email}"
+}
+resource "google_project_iam_member" "run_production_public_cloudbuild_builds_builder_on_production" {
+	project=google_project.production.project_id
+	role="roles/cloudbuild.builds.builder"
+	member="serviceAccount:${google_service_account.run_production_public.email}"
+}
+resource "google_project_iam_member" "run_production_public_cloudfunctions_developer_on_svpc_host" {
+	project=google_project.svpc_host.project_id
+	role="roles/cloudfunctions.developer"
+	member="serviceAccount:${google_service_account.run_production_public.email}"
+}
+resource "google_project_iam_member" "run_production_public_cloudfunctions_developer_on_production" {
+	project=google_project.production.project_id
+	role="roles/cloudfunctions.developer"
+	member="serviceAccount:${google_service_account.run_production_public.email}"
+}
+resource "google_project_iam_member" "run_production_public_cloudfunctions_developer_on_production_public" {
+	project=google_project.production_public.project_id
+	role="roles/cloudfunctions.developer"
+	member="serviceAccount:${google_service_account.run_production_public.email}"
+}
+resource "google_project_iam_member" "run_production_public_secretmanager_secret_accessor_on_production" {
+	project=google_project.production.project_id
+	role="roles/secretmanager.secretAccessor"
+	member="serviceAccount:${google_service_account.run_production_public.email}"
+}
+
+
 
